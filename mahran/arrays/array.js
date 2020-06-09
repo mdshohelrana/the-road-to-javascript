3ab1d980dd5eb5da7e296954a8d903d4e162d05a
Mahran Elmasri <mahran_masri@yahoo.com>
2020-06-09 15:12:22 +0200

    mahran-lesson-1: Resolve conflect

--- mahran/arrays/array.js
+++ mahran/arrays/array.js
@@ -1,3 +1,16 @@
+//Empty array
+const emptyArray = [];
+
+//Array of numbers
+const numbers = [10, 23, 11.5, -20];
+
+//Array of mixed types
+const data = ["Joe", 12345, true, "male"];
+
+let fruits = ["apple", "orange", "banana"];
+console.log(fruits[0]); // print first element "apple"
+
+console.log(fruits[fruits.length - 1]); // print last element "banana"
 let fruits = ["apple", "orange", "banana"];
 console.log(fruits[0]); // print first element "apple"
 
